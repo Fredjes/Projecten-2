53b34ceefddee194b50aa205caf630301d177601
Fredjes <frederik.de.smedt@hotmail.com>
2015-02-12 11:09:19 +0100

    Initial commit.

---
+++ src/gui/MainApp.java
@@ -0,0 +1,24 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Frederik
+ */
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	launch(args);
+    }
+
+}
