e85d50ff9c36cf7b4e819e3568e1e2875c7ca29e
Fredjes <frederik.de.smedt@hotmail.com>
2015-03-05 12:20:27 +0100

    Minor fixes.

--- src/gui/IconConfig.java
+++ src/gui/IconConfig.java
@@ -8,6 +8,5 @@ public class IconConfig {
 
     public static final String ITEM_ICON = "\uf02a";
     public static final String LOGOUT_ICON = "\uf08b";
-    public static final String BACK_ICON = "\uf137";
 
 }
