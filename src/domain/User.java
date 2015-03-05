4dd43a7c342a7ab1f2042cae5043fa0f428e98ed
Fredjes <frederik.de.smedt@hotmail.com>
2015-03-05 10:24:46 +0100

    Minor fixes.

--- src/domain/User.java
+++ src/domain/User.java
@@ -11,94 +11,85 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(name = "TBL_USER")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Access(AccessType.FIELD)
     private int id;
-    private StringProperty name = new SimpleStringProperty();
-    private StringProperty classRoom = new SimpleStringProperty();
-    private StringProperty email = new SimpleStringProperty();
-    private ObjectProperty<UserType> userType = new SimpleObjectProperty<>();
+
+    private final StringProperty name = new SimpleStringProperty();
+    private final StringProperty classRoom = new SimpleStringProperty();
+    private final StringProperty email = new SimpleStringProperty();
+    private final ObjectProperty<UserType> userType = new SimpleObjectProperty<>();
 
     public static enum UserType {
 
-        TEACHER("Leerkracht"), VOLUNTEER("Vrijwilliger"), STUDENT("Leerling");
+	TEACHER("Leerkracht"), VOLUNTEER("Vrijwilliger"), STUDENT("Leerling");
 
-        private final String translation;
+	private final String translation;
 
-        private UserType(String translation) {
-            this.translation = translation;
-        }
+	private UserType(String translation) {
+	    this.translation = translation;
+	}
 
-        @Override
-        public String toString() {
-            return translation;
-        }
-    };
+	@Override
+	public String toString() {
+	    return translation;
+	}
+    }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int getId() {
-        return id;
+    public UserType getUserType() {
+	return userType.get();
+    }
+
+    public void setUserType(UserType userType) {
+	this.userType.set(userType);
     }
 
     public String getName() {
-        return name.get();
+	return name.get();
     }
 
     public void setName(String name) {
-        this.name.set(name);
+	this.name.set(name);
     }
 
     public String getClassRoom() {
-        return classRoom.get();
+	return classRoom.get();
     }
 
     public void setClassRoom(String classRoom) {
-        this.classRoom.set(classRoom);
+	this.classRoom.set(classRoom);
     }
 
     public String getEmail() {
-        return email.get();
+	return email.get();
     }
 
     public void setEmail(String email) {
-        this.email.set(email);
+	this.email.set(email);
     }
 
-    /* Property getters & setters */
-    public StringProperty getNameProperty() {
-        return name;
+    /* Property */
+    public StringProperty nameProperty() {
+	return name;
     }
 
-    public void setNameProperty(StringProperty name) {
-        this.name = name;
+    public StringProperty classRoomProperty() {
+	return classRoom;
     }
 
-    public StringProperty getClassRoomProperty() {
-        return classRoom;
+    public ObjectProperty<UserType> userTypeProperty() {
+	return userType;
     }
 
-    public void setClassRoomProperty(StringProperty classRoom) {
-        this.classRoom = classRoom;
+    public StringProperty emailProperty() {
+	return email;
     }
-
-    public ObjectProperty<UserType> getUserTypeProperty() {
-        return userType;
-    }
-
-    public void setUserTypeProperty(ObjectProperty<UserType> userType) {
-        this.userType = userType;
-    }
-
-    public StringProperty getEmailProperty() {
-        return email;
-    }
-
-    public void setEmailProperty(StringProperty email) {
-        this.email = email;
-    }
-
 }
