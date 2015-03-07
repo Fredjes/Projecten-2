f2543106b93c791901531cb8d374b26b0bdc2fec
Pieter-Jan Geeroms <PieterjanGeeroms@hotmail.com>
2015-03-07 11:36:34 +0100

    Changed Dialogmanager method argruments (Node e) -> (Node parent, Node child) for multiple usage

--- src/gui/dialogs/DialogManager.java
+++ src/gui/dialogs/DialogManager.java
@@ -9,9 +9,15 @@ import org.controlsfx.control.PopOver;
  */
 public class DialogManager {
 
-    public static void showLogin(Node e) {
-        PopOver logPop = new PopOver(new LoginPanel());
+    /**
+     * Shows the popOver with content & parent of your choice
+     *
+     * @param parent Node to show popOver at.
+     * @param content Content to place in the popOver
+     */
+    public static void showPopOver(Node parent, Node content) {
+        PopOver logPop = new PopOver(content);
         logPop.setArrowLocation(PopOver.ArrowLocation.TOP_RIGHT);
-        logPop.show(e);
+        logPop.show(parent);
     }
 }
