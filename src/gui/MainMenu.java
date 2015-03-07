080bfdec30af96491237c912bf3e4b4963198c73
Pieter-Jan Geeroms <PieterjanGeeroms@hotmail.com>
2015-03-07 11:35:42 +0100

    Added dummy test for login

--- src/gui/MainMenu.java
+++ src/gui/MainMenu.java
@@ -1,5 +1,8 @@
 package gui;
 
+import gui.dialogs.DialogManager;
+import gui.dialogs.LoginPanel;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -13,39 +16,43 @@ public class MainMenu extends BorderPane {
 
     @FXML
     private Button loginButton;
-    
+
     @FXML
     private GridPane navigationGrid;
-    
+
     private ScreenSwitcher switcher;
 
     public MainMenu(ScreenSwitcher switcher) {
-	this.switcher = switcher;
-	FXUtil.loadFXML(this, "main_menu");
+        this.switcher = switcher;
+        FXUtil.loadFXML(this, "main_menu");
     }
 
     @FXML
     public void onManageUsers() {
-	switcher.openUserManagement();
+        switcher.openUserManagement();
     }
 
     @FXML
     public void onImportExcel() {
-	switcher.openExcelImport();
+        switcher.openExcelImport();
     }
 
     @FXML
     public void onManageLoans() {
-	switcher.openLoanManagement();
+        switcher.openLoanManagement();
     }
 
     @FXML
     public void onManageItems() {
-	switcher.openItemManagement();
+        switcher.openItemManagement();
     }
 
     @FXML
     public void onLogin() {
-	
+        LoginPanel p = new LoginPanel();
+        p.setOnLogin((ActionEvent e) -> {
+            System.out.printf("Username: %s%nPassword: %s%n", p.getUsername(), p.getPassword());
+        });
+        DialogManager.showPopOver(loginButton, p);
     }
 }
