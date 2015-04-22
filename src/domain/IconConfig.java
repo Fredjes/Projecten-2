e169b4d03475c6432645e8fa9663c72a5711aad7
Pieter-Jan Geeroms <PieterjanGeeroms@hotmail.com>
2015-04-22 18:36:16 +0200

    Added icons to the IconConfig

--- src/domain/IconConfig.java
+++ src/domain/IconConfig.java
@@ -14,15 +14,28 @@ import javafx.scene.text.Text;
 public class IconConfig {
 
     private static final HashMap<String, Icon> iconMapping = new HashMap() {
-	{
-	    put("icon-voorwerpen-beheren", new Icon("\uf1b3", IconType.TEXT, IconPosition.OVERWRITE));
-	    put("icon-uitleningen-beheren", new Icon("\uf02c", IconType.TEXT, IconPosition.OVERWRITE));
-	    put("icon-gebruikers-beheren", new Icon("\uf0c0", IconType.TEXT, IconPosition.OVERWRITE));
-	    put("icon-excel-importeren", new Icon("\uf0ce", IconType.TEXT, IconPosition.OVERWRITE));
-	    put("icon-te-laat", new Icon("\uf071", IconType.TEXT, IconPosition.OVERWRITE));
-	    put("icon-search", new Icon("\uf002", IconType.TEXT, IconPosition.OVERWRITE));
-	    put("icon-home", new Icon("\uf015", IconType.TEXT, IconPosition.OVERWRITE));
-	}
+        {
+            put("icon-voorwerpen-beheren", new Icon("\uf1b3", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-uitleningen-beheren", new Icon("\uf02c", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-gebruikers-beheren", new Icon("\uf0c0", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-excel-importeren", new Icon("\uf0ce", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-te-laat", new Icon("\uf071", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-search", new Icon("\uf002", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-home", new Icon("\uf015", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-add", new Icon("\uf0fe", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-delete", new Icon("\uf014", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-save", new Icon("\uf0c7", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-return", new Icon("\uf064", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-extend", new Icon("\uf017", IconType.TEXT, IconPosition.OVERWRITE));
+
+            put("icon-book", new Icon("\uf02d", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-game", new Icon("\uf091", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-cd", new Icon("\uf025", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-dvd", new Icon("\uf008", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-story", new Icon("\uf0b1", IconType.TEXT, IconPosition.OVERWRITE));
+            put("icon-all", new Icon("\uf1ce", IconType.TEXT, IconPosition.OVERWRITE));
+
+        }
     };
 
     /**
@@ -31,7 +44,7 @@ public class IconConfig {
      */
     public enum IconType {
 
-	GRAPHIC, TEXT
+        GRAPHIC, TEXT
     }
 
     /**
@@ -40,7 +53,7 @@ public class IconConfig {
      */
     public enum IconPosition {
 
-	PREPEND, APPEND, OVERWRITE
+        PREPEND, APPEND, OVERWRITE
     }
 
     /**
@@ -50,7 +63,7 @@ public class IconConfig {
      * @return The icon
      */
     public static Icon getIconFor(String cssClass) {
-	return iconMapping.get(cssClass);
+        return iconMapping.get(cssClass);
     }
 
     /**
@@ -61,29 +74,29 @@ public class IconConfig {
      * @param cssClass
      */
     public static void identify(Node node, String cssClass) {
-	if (!iconMapping.containsKey(cssClass)) {
-	    return;
-	}
+        if (!iconMapping.containsKey(cssClass)) {
+            return;
+        }
 
-	if (node instanceof Text) {
-	    Text target = (Text) node;
-	    Font iconFont = FontCache.getIconFont(target.getFont().getSize());
-	    target.setFont(iconFont);
+        if (node instanceof Text) {
+            Text target = (Text) node;
+            Font iconFont = FontCache.getIconFont(target.getFont().getSize());
+            target.setFont(iconFont);
 
-	    if (iconMapping.containsKey(cssClass)) {
-		Icon icon = iconMapping.get(cssClass);
-		setIcon(target, icon);
-	    }
-	} else if (node instanceof Labeled) {
-	    Labeled target = (Labeled) node;
-	    Font iconFont = FontCache.getIconFont(target.getFont().getSize());
-	    target.setFont(iconFont);
+            if (iconMapping.containsKey(cssClass)) {
+                Icon icon = iconMapping.get(cssClass);
+                setIcon(target, icon);
+            }
+        } else if (node instanceof Labeled) {
+            Labeled target = (Labeled) node;
+            Font iconFont = FontCache.getIconFont(target.getFont().getSize());
+            target.setFont(iconFont);
 
-	    if (iconMapping.containsKey(cssClass)) {
-		Icon icon = iconMapping.get(cssClass);
-		setIcon(target, icon);
-	    }
-	}
+            if (iconMapping.containsKey(cssClass)) {
+                Icon icon = iconMapping.get(cssClass);
+                setIcon(target, icon);
+            }
+        }
     }
 
     /**
@@ -93,22 +106,22 @@ public class IconConfig {
      * @param icon The {@link Icon} that should be used
      */
     public static void setIcon(Text text, Icon icon) {
-	switch (icon.getType()) {
-	    case GRAPHIC:
-	    case TEXT:
-		switch (icon.getPosition()) {
-		    case APPEND:
-			text.setText(text.getText() + icon.getIcon());
-			break;
-		    case OVERWRITE:
-			text.setText(icon.getIcon());
-			break;
-		    case PREPEND:
-			text.setText(icon.getIcon() + text.getText());
-			break;
-		}
-		break;
-	}
+        switch (icon.getType()) {
+            case GRAPHIC:
+            case TEXT:
+                switch (icon.getPosition()) {
+                    case APPEND:
+                        text.setText(text.getText() + icon.getIcon());
+                        break;
+                    case OVERWRITE:
+                        text.setText(icon.getIcon());
+                        break;
+                    case PREPEND:
+                        text.setText(icon.getIcon() + text.getText());
+                        break;
+                }
+                break;
+        }
     }
 
     /**
@@ -118,25 +131,25 @@ public class IconConfig {
      * @param icon The {@link Icon} that should be used
      */
     public static void setIcon(Labeled text, Icon icon) {
-	switch (icon.getType()) {
-	    case GRAPHIC:
-		Text t = new Text(icon.getIcon());
-		t.setFont(FontCache.getIconFont(12));
-		text.setGraphic(t);
-		break;
-	    case TEXT:
-		switch (icon.getPosition()) {
-		    case APPEND:
-			text.setText(text.getText() + icon.getIcon());
-			break;
-		    case OVERWRITE:
-			text.setText(icon.getIcon());
-			break;
-		    case PREPEND:
-			text.setText(icon.getIcon() + text.getText());
-			break;
-		}
-		break;
-	}
+        switch (icon.getType()) {
+            case GRAPHIC:
+                Text t = new Text(icon.getIcon());
+                t.setFont(FontCache.getIconFont(12));
+                text.setGraphic(t);
+                break;
+            case TEXT:
+                switch (icon.getPosition()) {
+                    case APPEND:
+                        text.setText(text.getText() + icon.getIcon());
+                        break;
+                    case OVERWRITE:
+                        text.setText(icon.getIcon());
+                        break;
+                    case PREPEND:
+                        text.setText(icon.getIcon() + text.getText());
+                        break;
+                }
+                break;
+        }
     }
 }
